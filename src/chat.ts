68e9268b63ccb44ebd21fd967dc87bd18fac4d3c
pepsi <pepsi00@protonmail.com>
2020-10-20 00:31:01 -0700

    improve /help output

--- src/chat.ts
+++ src/chat.ts
@@ -9,18 +9,20 @@ import { printUser } from './commands/user'
 import { printAirdcVersion } from './commands/airdc'
 
 const helpText = `
-      User commands
+        #######################
+        User commands
+        #######################
 
-      /uptime\tShow uptime (Client & System)\t\t\t(public, visible to everyone)
-      /speed\tShow current network Upload/Download speed\t\t\t(public, visible to everyone)
-      /os\t\tShow the operating system\t\t\t(public, visible to everyone)
-      /airdc\t\tShow AirDC++w version and client uptime \t\t\t(public, visible to everyone)
-      /stats\t\tShow various stats (Client, Uptime, Ratio, CPU)\t\t\t(public, visible to everyone)
-      /ratio\t\tShow Upload/Download stats\t\t\t(public, visible to everyone)
-      /sratio\tShow Session Upload/Download stats\t\t\t(public, visible to everyone)
-      /version\tShow user-commands extension version\t\t\t(private, visible only to yourself)
-      /user username\tSearch for a user and show the hub user was found on\t\t\t(private, visible only to yourself)
-      /list username /share/folder\tList all items within a users shared folder, writing items to local file\t\t\t(private, visible only to yourself)
+        /uptime\tShow uptime (Client & System)\t\t\t(public, visible to everyone)
+        /speed\tShow current network Upload/Download speed\t\t\t(public, visible to everyone)
+        /os\t\tShow the operating system\t\t\t(public, visible to everyone)
+        /airdc\t\tShow AirDC++w version and client uptime \t\t\t(public, visible to everyone)
+        /stats\t\tShow various stats (Client, Uptime, Ratio, CPU)\t\t\t(public, visible to everyone)
+        /ratio\t\tShow Upload/Download stats\t\t\t(public, visible to everyone)
+        /sratio\tShow Session Upload/Download stats\t\t\t(public, visible to everyone)
+        /version\tShow user-commands extension version\t\t\t(private, visible only to yourself)
+        /user username\tSearch for a user and show the hub user was found on\t\t\t(private, visible only to yourself)
+        /list username /share/folder\tList all items within a users shared folder, writing items to local file\t\t\t(private, visible only to yourself)
 `;
 
 // Basic chat command handling, returns possible status message to post
