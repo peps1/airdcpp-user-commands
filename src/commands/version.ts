9a3876bc5333ca40d91064add51a83924070ef8c
pepsi <pepsi00@protonmail.com>
2020-12-13 21:23:49 -0800

    update /version output

--- src/commands/version.ts
+++ src/commands/version.ts
@@ -2,14 +2,16 @@ import { printStatusMessage } from '../log';
 
 // /version command
 export const printVersion = async (type: string, entityId: string|number) => {
-  const extensions: any = await globalThis.SOCKET.get('extensions');
-  const sysinfoResults: any = await globalThis.SOCKET.get('system/system_info');
+  const extensions: any = await global.SOCKET.get('extensions');
+  const sysinfoResults: any = await global.SOCKET.get('system/system_info');
   const output: any = [];
-  output.push(`${sysinfoResults.client_version} - ${sysinfoResults.platform}`);
+  output.push('\n#######################');
+  output.push(`${sysinfoResults.client_version} - ${sysinfoResults.platform} - nodejs ${process.version}`);
+  output.push('#######################');
   output.push('Installed extensions:');
   extensions.forEach((ext: any) => {
     const status = (ext.running) ? 'running' : 'stopped';
-    output.push(`${ext.name} (${status}) [${ext.version}] published by ${ext.author} (${ext.homepage || 'homepage not set'})`);
+    output.push(`- [ ${ext.name} ] [${status}] [${ext.version}] published by ${ext.author} (${ext.homepage || 'homepage not set'})`);
   });
   // sendMessage(message, `Extension Version: ${output}`, type);
   printStatusMessage(output.join('\n'), type, entityId);
