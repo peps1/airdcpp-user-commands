5a2082d46fd3e891f945047ee162efc96d3008dc
pepsi <pepsi00@protonmail.com>
2020-12-14 10:44:23 -0800

    fix os output

--- src/commands/os.ts
+++ src/commands/os.ts
@@ -4,10 +4,5 @@ import { printEvent } from '../log';
 // /os command
 export const printOsInfo = async () => {
   const osInfoResult = Utils.getOsInfo();
-  if (osInfoResult[1].length !== 0) {
-    printEvent(`Error when getting OS info: ${osInfoResult[1]}`, 'error');
-    return;
-  } else {
-    return `-=[ Operating system: ${osInfoResult[0]} ]=-`;
-  }
+  return `-=[ Operating system: ${osInfoResult[0]} ]=-`;
 };
