bb6db23976b0a7862eddcd45bbdbe5cde88cd0ce
pepsi <pepsi00@protonmail.com>
2020-12-17 21:01:58 -0800

    change output of /airdc command

--- src/commands/airdc.ts
+++ src/commands/airdc.ts
@@ -1,12 +1,10 @@
 import { sendChatMessage } from '../chat';
-import * as Utils from '../utils';
 
 export const printAirdcVersion = async (type: string, entityId: string|number) => {
   const sysinfoResults: any = await global.SOCKET.get('system/system_info');
-  const uptime = sysinfoResults.client_started;
   const clientv = sysinfoResults.client_version;
 
-  const output = `-=[ ${clientv} (Uptime: ${Utils.formatUptime(Utils.clientUptime(uptime))}) ]=-`;
+  const output = `-=[ ${clientv} https://airdcpp-web.github.io/ ]=-`;
 
   sendChatMessage(output, type, entityId);
 };
